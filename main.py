5f218f58e012c05bbe3876f770af0104c881a499
Chase <covercas@uci.edu>
2022-05-30 22:11:09 -0700

    processes the NMNIST data and places it in a 2 list of events (a class), one list for train and one for test

--- main.py
+++ main.py
@@ -1,9 +1,30 @@
+import os
+import struct
 import numpy as np
 import pandas as pd
+from event import event
+
 
 def main():
-
+    train_events = parsedata(path = 'data\Train')
+    test_events = parsedata(path = 'data\Test')
     pass
+
+
+def parsedata(path):
+    events = []
+    try:
+        i = 0
+        for subdir in os.listdir(path):
+            subpath = os.path.join(path, subdir)
+            for file in os.listdir(subpath):
+                file_path = os.path.join(subpath, file)
+                e = event(path=file_path, target=i)
+                events.append(e)
+            i += 1
+        return events
+    except IOError:
+        print(IOError)
 
 
 if __name__ == '__main__':
