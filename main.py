783bc1a188503b4003a4f792142a1cedf35d540d
chase <covercas@asu.edu>
2022-05-31 17:57:50 -0700

    Event Image Count implementation

--- main.py
+++ main.py
@@ -2,29 +2,45 @@ import os
 import struct
 import numpy as np
 import pandas as pd
-from event import event
+
+import h5py
 
 
 def main():
-    train_events = parsedata(path = 'data\Train')
-    test_events = parsedata(path = 'data\Test')
+    # SEE READ ME FOR DATA LINKS
+    data = h5py.File('./data/indoor_flying4_data.hdf5')
+    gt_data = h5py.File('./data/indoor_flying4_gt.hdf5')
+    # ACCESS EVENTS (NEUROMORPHIC DATA) FROM LEFT CAMERA
+    left_data = data['davis']['left']['events']
+    # ACCESS EVENTS (NEUROMORPHIC DATA) FROM RIGHT CAMERA
+    right_data = data['davis']['right']['events']
+    X = left_data[:, :]
+    # Time Stamps of the Raw Images
+    ir_ts = data['davis']['right']['image_raw_ts']
+    # Raw Images
+    ir = data['davis']['right']['image_raw']
+    # print(list(data['davis']['right'].keys()))
+    event_image = event_count(X, ir_ts)
+    print(event_image[0, :, :])
     pass
 
 
-def parsedata(path):
-    events = []
-    try:
-        i = 0
-        for subdir in os.listdir(path):
-            subpath = os.path.join(path, subdir)
-            for file in os.listdir(subpath):
-                file_path = os.path.join(subpath, file)
-                e = event(path=file_path, target=i)
-                events.append(e)
-            i += 1
-        return events
-    except IOError:
-        print(IOError)
+# Creates a 3 dimensional array of event counts for each pixel
+def event_count(X, timestamps):
+    # make 3d array
+    event_count_image = np.zeros((622, 346, 260))
+    # iterate through each time stamp (1 raw image per time stamp)
+    for time in range(len(timestamps)):
+        index = 0
+        # iterate through all events prior to the next timestamp
+        while X[index, 2] <= timestamps[time + 1]:
+            # create a 2d matrix that counts the number of events at that pixel per time period
+            x = int(X[index][0])
+            y = int(X[index][1])
+            # iterate the event counter associated with the x and y axis of the event
+            event_count_image[time, x, y] += 1
+            index += 1
+    return event_count_image
 
 
 if __name__ == '__main__':
